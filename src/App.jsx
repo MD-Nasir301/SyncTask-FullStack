af4aa5b48b1ab19c320b8dc3a00ec65d68207eee
nasir21b@gmail.com <nasir21b@gmail.com>
2026-02-18 02:09:19 +0600

    Done: Header section style

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="h-screen bg-black flex items-center justify-center">
-      <h1 className="text-5xl font-extrabold text-orange-500">
-        Setup Success! 🚀
-      </h1>
+    <div>
+      <div className="min-h-screen bg-slate-800 p-4">
+        <header className="flex justify-between items-center bg-slate-600 p-4 rounded-2xl ">
+          <div>TS</div>
+          <div>ToDo</div>
+          <div className="text-white text-sm flex gap-3 items-center bg-slate-500 px-3 py-1 rounded-full cursor-pointer">
+            <span>LogIn</span>
+            <div className="w-8 h-8 bg-slate-300 rounded-full border border-orange-400"></div>
+          </div>
+        </header>
+
+      </div>
     </div>
   );
 }
