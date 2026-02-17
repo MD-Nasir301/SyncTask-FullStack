922f9656c52f8469e582f9750356b20e30a515b1
nasir21b@gmail.com <nasir21b@gmail.com>
2026-02-18 03:43:46 +0600

    feat:add  Date and time section  with functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,16 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(timer); // For my understand -- Clean up the timer when the component unmounts
+  }, []);
+
   return (
     <div>
       <div className="min-h-screen bg-slate-800 p-4">
@@ -10,6 +22,17 @@ function App() {
             <div className="w-8 h-8 bg-slate-300 rounded-full border border-orange-400"></div>
           </div>
         </header>
+        <div className="flex justify-between mt-6 px-2 text-slate-400 font-mono text-sm md:text-lg">
+          <di>
+            <span className="text-yellow-400">Time:</span>{" "}
+            {currentTime.toLocaleTimeString()}
+          </di>
+          <div>
+            <span className="text-yellow-400">Date:</span>{" "}
+            {currentTime.toLocaleDateString()}
+          </div>
+        </div>
+
 
       </div>
     </div>
