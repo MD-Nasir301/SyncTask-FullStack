bdca3e0d0e2f93cf536292479e40a0c08aa3ab22
nasir21b@gmail.com <nasir21b@gmail.com>
2026-02-18 06:25:05 +0600

    feat: handleAddTask function created

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,30 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [currentTime, setCurrentTime] = useState(new Date());
+  const [inputValue, setInputValue] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  const handleAddTask = () => {
+    if (inputValue.length === 0) {
+      return;
+    }
+    const task = {
+      id: Date.now(),
+      text: inputValue,
+      date: currentTime.toLocaleDateString(),
+      isCompleted: false,
+      isSynced: false,
+    };
+
+    setTasks((prevTasks) => [...prevTasks, task]);
+    setInputValue("");
+  };
+  console.log(tasks);
 
   useEffect(() => {
     const timer = setInterval(() => {
       setCurrentTime(new Date());
-    }, 1000);
+    }, 10000000);
 
     return () => clearInterval(timer); // For my understand -- Clean up the timer when the component unmounts
   }, []);
@@ -37,9 +56,17 @@ function App() {
           <div className="flex gap-2">
             <input
               type="text"
+              value={inputValue}
+              onChange={(e) => {
+                setInputValue(e.target.value);
+              }}
+              placeholder="Type Your Task..."
               className="flex-1 bg-slate-800 border-2 border-slate-700 rounded-xl px-5 py-3 text-white focus:outline-none focus:border-orange-500 transition-all   "
             />
-            <button className=" bg-orange-400 px-6 text-white font-bold rounded-2xl hover:bg-amber-400 transition-all shadow-lg cursor-pointer hover:text-black active:scale-95">
+            <button
+              onClick={handleAddTask}
+              className=" bg-orange-400 px-6 text-white font-bold rounded-2xl hover:bg-amber-400 transition-all shadow-lg cursor-pointer hover:text-black active:scale-95"
+            >
               Add
             </button>
           </div>
@@ -50,29 +77,35 @@ function App() {
             <h3 className="text-slate-500 font-bold mb-4 border-b border-slate-400 pb-2 text-sm md:text-lg">
               🗓️ 20-02-2026
             </h3>
-
-            <div className="flex single-task mb-3  gap-4 justify-between border-l-4 border-orange-400 group transition-all hover:border-white p-3 rounded-xl bg-slate-900">
-              <div className="flex-1 text-white text-lg font-medium ">
-                <div className="text-[16px] text-justify text-yellow-200">
-                  আমার নাম জেমস, আমি একজন সফটওয়্যার ডেভেলপার।
-                </div>
-                <span className="text-[10px]  bg-blue-500/10 mt-1 text-blue-300 px-2 py-0.5 rounded-md">
-                  ☁️ Cloud Synced
-                </span>
-                {/* <span className="text-[10px] cursor-pointer bg-amber-500/20 mt-1 text-amber-500 px-2 py-0.5 rounded-md hover:bg-amber-500 hover:text-white transition-all">
+            {tasks.map((task) => {
+              return (
+                <div
+                  key={task.id}
+                  className="flex single-task mb-3  gap-4 justify-between border-l-4 border-orange-400 group transition-all hover:border-white p-3 rounded-xl bg-slate-900"
+                >
+                  <div className="flex-1 text-white text-lg font-medium ">
+                    <div className="text-[16px] text-justify text-yellow-200">
+                      {task.text}
+                    </div>
+                    <span className="text-[10px]  bg-blue-500/10 mt-1 text-blue-300 px-2 py-0.5 rounded-md">
+                      ☁️ Cloud Synced
+                    </span>
+                    {/* <span className="text-[10px] cursor-pointer bg-amber-500/20 mt-1 text-amber-500 px-2 py-0.5 rounded-md hover:bg-amber-500 hover:text-white transition-all">
                   ⚠️ Not Synced (Click to Upload)
                 </span> */}
-              </div>
+                  </div>
 
-              <div className="flex flex-col gap-2">
-                <button className="border  px-6 text-orange-400 font-bold rounded-2xl hover:bg-amber-400 transition-all shadow-lg cursor-pointer hover:text-black active:scale-95">
-                  Done
-                </button>
-                <button className="border border-orange-400 px-6 text-orange-400 font-bold rounded-2xl hover:bg-amber-400 transition-all shadow-lg cursor-pointer hover:text-black active:scale-95">
-                  Edit
-                </button>
-              </div>
-            </div>
+                  <div className="flex flex-col gap-2">
+                    <button className="border  px-6 text-orange-400 font-bold rounded-2xl hover:bg-amber-400 transition-all shadow-lg cursor-pointer hover:text-black active:scale-95">
+                      Done
+                    </button>
+                    <button className="border border-orange-400 px-6 text-orange-400 font-bold rounded-2xl hover:bg-amber-400 transition-all shadow-lg cursor-pointer hover:text-black active:scale-95">
+                      Edit
+                    </button>
+                  </div>
+                </div>
+              );
+            })}
           </section>
         </div>
       </div>
