1569c22f6ad2a3ddae1e063f2f7029a6ac604be8
nasir21b@gmail.com <nasir21b@gmail.com>
2026-02-19 00:56:51 +0600

    feat: add grouped task list with nested mapping

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,10 @@ import { useEffect, useState } from "react";
 function App() {
   const [currentTime, setCurrentTime] = useState(new Date());
   const [inputValue, setInputValue] = useState("");
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState(() => {
+    const savedTasks = localStorage.getItem("tasks");
+    return savedTasks ? JSON.parse(savedTasks) : [];
+  });
 
   const handleAddTask = () => {
     if (inputValue.length === 0) {
@@ -20,7 +23,10 @@ function App() {
     setTasks((prevTasks) => [...prevTasks, task]);
     setInputValue("");
   };
-  console.log(tasks);
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
 
   useEffect(() => {
     const timer = setInterval(() => {
@@ -29,6 +35,21 @@ function App() {
 
     return () => clearInterval(timer); // For my understand -- Clean up the timer when the component unmounts
   }, []);
+
+  const groupedTasks = tasks.reduce((groups, task) => {
+    const { date } = task;
+    if (!groups[date]) {
+      groups[date] = [];
+    }
+    groups[date].push(task);
+    return groups;
+  }, {});
+
+  const dates = Object.keys(groupedTasks);
+
+  const sortDates = dates.sort((a, b) => {
+    return new Date(b) - new Date(a);
+  });
 
   return (
     <div>
@@ -42,13 +63,13 @@ function App() {
           </div>
         </header>
         <div className="flex justify-between mt-4 px-2 text-slate-400 font-mono text-sm md:text-lg">
-          <di>
+          <div>
             <span className="text-yellow-400">Time:</span>{" "}
             {currentTime.toLocaleTimeString()}
-          </di>
+          </div>
           <div>
             <span className="text-yellow-400">Date:</span>{" "}
-            {currentTime.toLocaleDateString()}
+            {new Intl.DateTimeFormat("en-GB").format(new Date(currentTime))}
           </div>
         </div>
 
@@ -73,40 +94,44 @@ function App() {
         </div>
 
         <div className="max-w-3xl mx-auto mt-8 space-y-8">
-          <section>
-            <h3 className="text-slate-500 font-bold mb-4 border-b border-slate-400 pb-2 text-sm md:text-lg">
-              🗓️ 20-02-2026
-            </h3>
-            {tasks.map((task) => {
-              return (
-                <div
-                  key={task.id}
-                  className="flex single-task mb-3  gap-4 justify-between border-l-4 border-orange-400 group transition-all hover:border-white p-3 rounded-xl bg-slate-900"
-                >
-                  <div className="flex-1 text-white text-lg font-medium ">
-                    <div className="text-[16px] text-justify text-yellow-200">
-                      {task.text}
-                    </div>
-                    <span className="text-[10px]  bg-blue-500/10 mt-1 text-blue-300 px-2 py-0.5 rounded-md">
-                      ☁️ Cloud Synced
-                    </span>
-                    {/* <span className="text-[10px] cursor-pointer bg-amber-500/20 mt-1 text-amber-500 px-2 py-0.5 rounded-md hover:bg-amber-500 hover:text-white transition-all">
+          {sortDates.map((date) => {
+            return (
+              <section key={date}>
+                <h3 className="text-slate-500 font-bold mb-4 border-b border-slate-400 pb-2 text-sm md:text-lg">
+                  🗓️ {new Intl.DateTimeFormat("en-GB").format(new Date(date))}
+                </h3>
+                {groupedTasks[date].map((task) => {
+                  return (
+                    <div
+                      key={task.id}
+                      className="flex single-task mb-3  gap-4 justify-between border-l-4 border-orange-400 group transition-all hover:border-white p-3 rounded-xl bg-slate-900"
+                    >
+                      <div className="flex-1 text-white text-lg font-medium ">
+                        <div className="text-[16px] text-justify text-yellow-200">
+                          {task.text}
+                        </div>
+                        <span className="text-[10px]  bg-blue-500/10 mt-1 text-blue-300 px-2 py-0.5 rounded-md">
+                          ☁️ Cloud Synced
+                        </span>
+                        {/* <span className="text-[10px] cursor-pointer bg-amber-500/20 mt-1 text-amber-500 px-2 py-0.5 rounded-md hover:bg-amber-500 hover:text-white transition-all">
                   ⚠️ Not Synced (Click to Upload)
                 </span> */}
-                  </div>
+                      </div>
 
-                  <div className="flex flex-col gap-2">
-                    <button className="border  px-6 text-orange-400 font-bold rounded-2xl hover:bg-amber-400 transition-all shadow-lg cursor-pointer hover:text-black active:scale-95">
-                      Done
-                    </button>
-                    <button className="border border-orange-400 px-6 text-orange-400 font-bold rounded-2xl hover:bg-amber-400 transition-all shadow-lg cursor-pointer hover:text-black active:scale-95">
-                      Edit
-                    </button>
-                  </div>
-                </div>
-              );
-            })}
-          </section>
+                      <div className="flex flex-col gap-2">
+                        <button className="border  px-6 text-orange-400 font-bold rounded-2xl hover:bg-amber-400 transition-all shadow-lg cursor-pointer hover:text-black active:scale-95">
+                          Done
+                        </button>
+                        <button className="border border-orange-400 px-6 text-orange-400 font-bold rounded-2xl hover:bg-amber-400 transition-all shadow-lg cursor-pointer hover:text-black active:scale-95">
+                          Edit
+                        </button>
+                      </div>
+                    </div>
+                  );
+                })}
+              </section>
+            );
+          })}
         </div>
       </div>
     </div>
