f58bfbbdd09c3d3f4b6ff9516522eb1686a0d512
nasir21b@gmail.com <nasir21b@gmail.com>
2026-04-21 02:11:21 +0600

    test: added header menu click option for touch device

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,25 @@
+import { useState } from "react";
 import demoProfile from "../assets/demo-profile-img.png";
 import taskImg from "../assets/task.png";
 
+const [isMenuOpen, setIsMenuOpen] = useState(false);
+
 function Header({ user, handleLogin, handleLogout, filterData }) {
-  console.log("..........Header...........")
+  console.log("..........Header...........");
   return (
     <header className="flex justify-between items-center bg-slate-600 p-3 rounded-2xl ">
-      <div><img className="w-10 h-10" src={taskImg} alt="" /> </div>
-      <div className="text-xl font-bold text-orange-400 text-shadow-red-600 text-shadow-lg">To <span className="inline-block  rotate-5">Do</span></div>
+      <div>
+        <img className="w-10 h-10" src={taskImg} alt="" />{" "}
+      </div>
+      <div className="text-xl font-bold text-orange-400 text-shadow-red-600 text-shadow-lg">
+        To <span className="inline-block  rotate-5">Do</span>
+      </div>
       <div className="flex items-center ">
         {user ? (
-          <div className="text-white text-sm relative group flex gap-3 items-center bg-slate-500 px-3 py-1 rounded-full cursor-pointer">
+          <div
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
+            className="text-white text-sm relative group flex gap-3 items-center bg-slate-500 px-3 py-1 rounded-full cursor-pointer"
+          >
             {" "}
             <span className="flex-1 text-sm md:text-lg text-slate-900">
               {user.displayName}
@@ -24,7 +34,7 @@ function Header({ user, handleLogin, handleLogout, filterData }) {
             </div>
             <button
               onClick={handleLogout}
-              className="cursor-pointer w-full top-full right-0 hidden group-hover:block bg-slate-800  border-2 font-bold text-[14px] px-2 py-2 left-0 rounded-2xl absolute"
+              className="cursor-pointer  w-full top-full ${isMenuOpen ? 'block' : 'hidden '} right-0 hidden group-hover:block group-active:block bg-slate-800  border-2 font-bold text-[14px] px-2 py-2 left-0 rounded-2xl absolute"
             >
               Logout
             </button>
@@ -32,7 +42,12 @@ function Header({ user, handleLogin, handleLogout, filterData }) {
         ) : (
           <div className="text-white text-sm flex gap-3 items-center  bg-slate-500 px-3 py-1 rounded-full cursor-pointer">
             {" "}
-            <span className="hover:bg-gray-600 px-3 py-0.5 rounded-2xl" onClick={handleLogin}>LogIn</span>
+            <span
+              className="hover:bg-gray-600 px-3 py-0.5 rounded-2xl"
+              onClick={handleLogin}
+            >
+              LogIn
+            </span>
             <div className="w-8 h-8 bg-slate-300 rounded-full border overflow-hidden border-orange-400">
               <img className="w-full" src={demoProfile} alt="" />
             </div>
@@ -43,13 +58,22 @@ function Header({ user, handleLogin, handleLogout, filterData }) {
             ...
           </span>
           <ul className="absolute  hidden top-full right-0 py-5 pt-4 w-50  group-hover:block ">
-            <li onClick={()=>filterData("local")} className="bg-black px-6 py-1 border-2 border-transparent hover:border-blue-300">
+            <li
+              onClick={() => filterData("local")}
+              className="bg-black px-6 py-1 border-2 border-transparent hover:border-blue-300"
+            >
               Only Local Data
             </li>
-            <li  onClick={()=>filterData("cloud")} className="bg-green-500 text-black px-6 py-1 border-2 border-transparent hover:border-blue-300">
+            <li
+              onClick={() => filterData("cloud")}
+              className="bg-green-500 text-black px-6 py-1 border-2 border-transparent hover:border-blue-300"
+            >
               Only Database Data
             </li>
-            <li onClick={()=>filterData("all")} className="bg-slate-500 text-black px-6 py-1 border-2 border-transparent hover:border-blue-300">
+            <li
+              onClick={() => filterData("all")}
+              className="bg-slate-500 text-black px-6 py-1 border-2 border-transparent hover:border-blue-300"
+            >
               All Data
             </li>
           </ul>
